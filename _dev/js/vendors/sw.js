7b612e7bc81e8a81ab405dd1c1b0fb043430d2ef
ivanheral <ivanhernandezal@gmail.com>
2019-03-01 17:33:10 +0100

    changes

--- _dev/js/vendors/sw.js
+++ _dev/js/vendors/sw.js
@@ -8,28 +8,10 @@ const offlineUrl = 'offline-page.html';
 
 this.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
+    caches.open(currentCache.offline).then(function (cache) {
       return cache.addAll([
-          offlineUrl
+        offlineUrl
       ]);
     })
   );
-});
-
-this.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
 });
